4ea00bf4573b3c6232d1dc4d7b5cb569c6d59d57
mordelonautomotriz <ector-line98@hotmail.com>
2020-06-05 23:12:24 -0500

    primera version_POO

---
+++ RequerimientosFuncionales.java
@@ -0,0 +1,46 @@
+/*
+disene un algoritmo en el que ingrese 
+los requerimientos funcionales
+del paper selecionado
+ */
+package requerimientosfuncionales;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author HECTOR ORTIZ
+ */
+public class RequerimientosFuncionales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner objeto=new Scanner(System.in);//new reserva espacio de memoria
+      
+       System.out.println("\tUniversidad De Las Fuerzas Armadas ESPE - Extension Latacunga ");
+ 
+        System.out.println("\nDepartamento: Energia y Mecanica");
+        
+        System.out.println("Carrera: Ingeniera Automotriz");
+        
+        System.out.println("Docente: Ing. Luis Guerra ");
+        
+        System.out.println("Asignatura: Programacion ");
+        
+        System.out.println("Nivel: 4to nivel 'B' ");
+        
+        System.out.println("ID: L00280605 \n");
+        
+        System.out.println("ingrese los REQUERIMIENTOS FUNCIONALES: \n");
+        
+        System.out.println("Propiedades \n");
+        String requerimientoun=objeto.nextLine();
+        
+        
+       
+    }
+    
+}
